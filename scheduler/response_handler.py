3f3f2e67123d12da7a7887f28683650c13e8e7af
Abhishek Lekshmanan <abhishek@suse.com>
2019-01-25 13:36:00 +0100

    response_handler: use needs_data to specifiy whether data needs to be read
    Signed-Off-By: Abhishek Lekshmanan <abhishek@suse.com>

--- scheduler/response_handler.py
+++ scheduler/response_handler.py
@@ -5,15 +5,26 @@ from collections import Counter
 class ResponseHandler(abc.ABC):
 
     @abc.abstractmethod
-    def handle_response(self, res):
+    def handle_response(self, response, response_data):
         pass
 
+    def needs_data(self):
+        return False
+
 class CounterHandler(ResponseHandler):
+    '''
+    A very basic request status counter, aggregating the total requests per
+    status code
+    '''
+
     def __init__(self):
         self.counter=Counter()
 
-    def handle_response(self, res):
-        self.counter[res.status] += 1
+    def handle_response(self, response, *_):
+        self.counter[response.status] += 1
+
+    def needs_data(self):
+        return True
 
     def print_stats(self):
         for k,v in self.counter.items():
