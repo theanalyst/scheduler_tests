2114b03e53ad70b0a71841a786b4541dc89284c0
Abhishek Lekshmanan <abhishek@suse.com>
2019-04-05 14:58:41 +0200

    make admin socket a generic cmd interface
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- scheduler/admin_socket.py
+++ scheduler/admin_socket.py
@@ -5,16 +5,21 @@ import struct
 import logging
 
 async def async_perf_dump(sock_path):
+    perf_dump = await admin_sock_cmd(sock_path, {"prefix": "perf dump"})
+    print(perf_dump['simple-throttler'])
+
+async def admin_sock_cmd(sock_path, cmd_dict):
+    # TODO: can we ctx mgr here?
     reader, writer = await asyncio.open_unix_connection(path=sock_path)
-    d = {"prefix": "perf dump"}
-    bstr = json.dumps(d).encode() + b'\0'
-    writer.write(bstr)
+    cmd_str = json.dumps(cmd_dict).encode() + b'\0'
+    writer.write(cmd_str)
     await writer.drain()
 
     data_len_b = await reader.read(n=4)
     data_len = int.from_bytes(data_len_b, byteorder='big')
 
-    perf_dump_b = await reader.read(data_len)
-    perf_dump = json.loads(perf_dump_b)
-    print(perf_dump['simple-throttler'])
+    data_b = await reader.read(data_len)
+    data_json = json.loads(data_b)
     writer.close()
+
+    return data_json
